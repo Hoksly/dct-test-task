f0303cc609be30e0643d6e1677ccb7234dae9e14
hoksly <realhoksly@gmail.com>
2024-03-16 16:40:17 +0200

    Market Request Parameters added

---
+++ ConsoleAPITestApp/Models/CoinCap/RequestParameters/MarketRequestParameters.cs
@@ -0,0 +1,9 @@
+namespace ConsoleAPITestApp.Models
+{
+    public class MarketRequestParameters 
+    {
+        public int? Limit { get; set; }
+        public int? Offset { get; set; }
+    }
+
+}
